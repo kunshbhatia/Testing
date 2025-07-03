a8d7ad99216ea757815346c6d64f4d06530118a0
kunshbhatia <bhatiakunsh@gmail.com>
2025-07-03 20:22:38 +0530

    Mofication of Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig: # Creation of the paths for the data present
+    train_data_path = os.path.join("artifacts","train.csv")
+    test_data_path = os.path.join("artifacts","test.csv")
+    raw_data_path = os.path.join("artifacts","raw.csv")
+
+class DataIngestion:
+    def __init__(self): # helps in getting the access to that train ,test and raw data file paths
+        self.ingestion_config = DataIngestionConfig()
+
+    def initate_data_ingestion(self):  # creates train , test and raw data into the file paths mentioned above
+        logging.info("Data Entery Method Has Been Started")
+        try:
+            df = pd.read_csv("data.csv")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info('Train Test Started')
+
+            train_set,test_set = train_test_split(df,test_size=0.20)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion Of Data Has Been Completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initate_data_ingestion()
+            
